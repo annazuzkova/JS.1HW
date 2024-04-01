73b780093a05d3d500bf1be7db17eba42f72d6e1
AnnaZuzkova <famzu2018@gmail.com>
2024-04-01 19:09:50 +0300

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// const age = 14;
+// console.log(age);
+// const name = 'Anna';
+// console.log(name);
+// const isStudent = true;
+// console.log(isStudent);
+// const myString = 'давайте уявимо що тут цитата';
+// console.log(myString);
+// let myNumber = 10;
+// myNumber = myNumber + 10;
+// console.log(myNumber)
+// const myNull = null;
+// console.log(myNull)
+// const nameUser = prompt('привіт! як вас звати?');
+// console.log(nameUser)
+// const myConfirm = confirm('вас так звати', nameUser)
+// const myAlarm= alert('Ця дія е небезпечною. продовжити')
